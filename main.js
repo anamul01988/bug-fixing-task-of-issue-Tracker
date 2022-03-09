c1850bb2b8712ef9dabc88735e1990bf6c300a0b
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-03-09 23:46:59 +0600

    all bug fixed and built local and session storage

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//setStatusClosed( ) will work as local storage jeita browser theke chole gele o applicaton er local storage a thekke jabe
+//deleteIssue() will work as sessoin stroge jeita browser theke chole gele , application er local storage theke o chole jabe
 document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
 
 function submitIssue(e) {
@@ -10,27 +12,53 @@ function submitIssue(e) {
 
   const issue = { id, description, severity, assignedTo, status };
   let issues = [];
-  if (localStorage.getItem('issues')){ //server a kicu na thakle 
+  if (localStorage.getItem('issues')){ 
     
     issues = JSON.parse(localStorage.getItem('issues'));
     console.log(issues)
   }
   issues.push(issue);
-  localStorage.setItem('issues', JSON.stringify(issues)); //aita khali cilo tai ekta key pathacce amra jani server pair hishebe orthat key value kore data rakhe . aikhane key ta pathalo issues nam diye 
+  localStorage.setItem('issues', JSON.stringify(issues)); //server khali cilo tai ekta key pathacce amra jani server pair hishebe orthat key value kore data rakhe . aikhane key ta pathalo issues nam diye 
   document.getElementById('issueInputForm').reset();
   fetchIssues();
   e.preventDefault();
 }
 
+
+// // ========= one way of using map=========
+// // const closeIssue = id => {
+//   const setStatusClosed = id => {
+//   const issues = JSON.parse(localStorage.getItem('issues'));
+//   console.log(issues)
+//   // const currentIssue = issues.find(issue => issue.id === id); 
+//   // const currentIssue = issues.map(issue => {
+//     const newIssues = issues.map(issue => {
+//       if(issue.id == id){
+//        issue.status = 'Closed';
+//      }
+//      return issue;
+//   } );
+//   console.log(newIssues)
+//   // console.log(currentIssue)
+//   // currentIssue.status = 'Closed';
+//   localStorage.setItem('issues', JSON.stringify(newIssues));
+//   fetchIssues();
+// }
+
+
+
+//========= another way of using find ===========
 // const closeIssue = id => {
   const setStatusClosed = id => {
-  const issues = JSON.parse(localStorage.getItem('issues'));
-  // const currentIssue = issues.find(issue => issue.id === id); find to 1 ta dibe jeita first pabe but filter sob gula dibe
-  const currentIssue = issues.filter(issue => issue.id != id);
-  currentIssue.status = 'Closed';
-  localStorage.setItem('issues', JSON.stringify(issues));
-  fetchIssues();
-}
+    const issues = JSON.parse(localStorage.getItem('issues'));
+    console.log(issues)
+    const currentIssue = issues.find(issue => +issue.id === id); 
+    currentIssue.status = 'Closed';
+    localStorage.setItem('issues', JSON.stringify(issues));
+    // localStorage.removeItem(issues);
+    fetchIssues();  //Closed holei fetchIssues er if er status condition continue korbe 
+
+  }
 
 const deleteIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
@@ -50,8 +78,8 @@ const fetchIssues = () => {
 
   for (var i = 0; i < issues.length; i++) {
     const {id, description, severity, assignedTo, status} = issues[i];
-
-    issuesList.innerHTML +=   `<div class="well">
+    if(status != 'Closed'){
+      issuesList.innerHTML +=   `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
                               <h3> ${description} </h3>
@@ -60,5 +88,8 @@ const fetchIssues = () => {
                               <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
+
+    }
+    
   }
 }
